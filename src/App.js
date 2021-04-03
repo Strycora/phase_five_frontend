da1e054ac31ec39f6a1ae634fc4b30c77fdfaf8e
Vikram Chatterjee <strycora@gmail.com>
2021-04-03 13:07:58 -0600

    made app a class component, cleaned up some code and made a practice fetch request in app, added dependencies in index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component {
+
+componentDidMount(){
+  fetch('http://localhost:3001/sessions')
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
+  }
 }
 
 export default App;
