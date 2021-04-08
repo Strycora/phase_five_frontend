b85029b55c5af73d4c5562c2c2a6232b988378e4
Vikram Chatterjee <strycora@gmail.com>
2021-04-08 11:23:51 -0600

    styled sessionInput Component

--- src/components/SessionInput.js
+++ src/components/SessionInput.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {addSession} from '../actions/addSession'
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core'
+import { Button } from '@material-ui/core';
+
+
+
+
+
+// const useStyles = makeStyles ({
+//   field: {
+//     marginTop: 20,
+//     marginBottom: 20,
+//     display: 'block'
+//   }
+// })
+// const classes = useStyles()
 
 class SessionInput extends React.Component{
+
+  
 
   state = {
     name: '',
@@ -27,12 +45,12 @@ class SessionInput extends React.Component{
   render(){
    return (
     <div>
-      <form onSubmit={this.handleSubmit}>
-        <label>Session Name:</label>
-        <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br></br>
-        <label>Session Date:</label>
-        <input type="date" placeholder="Date" name="date" value={this.state.date} onChange={this.handleChange}/><br></br>
-        <input type="submit" />
+      <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+        <TextField label="Session Name" variant="outlined" color="secondary" name="name" value={this.state.name} onChange={this.handleChange}></TextField>
+        {/* <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/><br></br> */}
+        <TextField id="date" label="Session Date" type="date" InputLabelProps={{shrink: true,}} name="date" value={this.state.date} onChange={this.handleChange} />
+        {/* <input type="date" placeholder="Date" name="date" value={this.state.date} onChange={this.handleChange}/><br></br> */}
+        <Button variant="contained" color="primary" type="submit">Submit</Button>
       </form>
     </div>
    )
