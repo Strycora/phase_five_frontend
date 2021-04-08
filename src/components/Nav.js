81064a98bd2f3757687190073b5548a128ecb59c
Vikram Chatterjee <strycora@gmail.com>
2021-04-08 10:55:34 -0600

    added roboto font to index.html, added material ui icons to dependencies, and styled nav and sessions.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import { AppBar, Toolbar } from '@material-ui/core';
+
 
 export class Nav extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/sessions'>Sessions</Link></li>
-          <li><Link to='/sessions/new'>New Session</Link></li>
-          <li><Link to='/bests'>Personal Bests</Link></li>
-          <li><Link to='/bests/new'>New Personal Best</Link></li>
-          <li><Link to='/inspirations'>My Inspirations</Link></li>
-          <li><Link to='/inspirations/new'>Add an Inspiration</Link></li>
-
-        </ul>
+        <AppBar position="static">
+          <Toolbar>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/sessions'>Sessions</Link></li>
+            <li><Link to='/sessions/new'>New Session</Link></li>
+            <li><Link to='/bests'>Personal Bests</Link></li>
+            <li><Link to='/bests/new'>New Personal Best</Link></li>
+            <li><Link to='/inspirations'>My Inspirations</Link></li>
+            <li><Link to='/inspirations/new'>Add an Inspiration</Link></li>
+          </Toolbar>
+        </AppBar>
+        
       </div>
     )
   }
--- src/components/Sessions.js
+++ src/components/Sessions.js
@@ -3,10 +3,22 @@ import {Route, Link} from 'react-router-dom'
 import Session from './Session'
 import {connect} from 'react-redux'
 import {deleteSession} from '../actions/deleteSession'
+import { makeStyles } from '@material-ui/core/styles';
+import { List, ListItem } from '@material-ui/core';
 // import {fetchSessions} from '../actions/fetchSessions'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: "green",
+  },
+}));
 
 const Sessions = (props) => {
 
+
+  const classes = useStyles();
   console.log(props);
 
   const handleDelete = (session) => {
@@ -14,13 +26,14 @@ const Sessions = (props) => {
   }
   
   return (
-    <div>
+    <div className={classes.root}>
+      <List component="nav" aria-label="main">
       {props.sessions.map(session => 
-        <li key={session.id}>
-          <Link to={`/sessions/${session.id}`}>{session.name}</Link><button onClick={() => handleDelete(session)}>Delete</button></li> )} 
+          <ListItem button><Link to={`/sessions/${session.id}`}> {session.name}</Link><button onClick={() => handleDelete(session)}>Delete</button>  </ListItem>)} 
+          </List>
     </div>
   )
-
+  // 
 }
 
 export default connect(null, {deleteSession})(Sessions)
